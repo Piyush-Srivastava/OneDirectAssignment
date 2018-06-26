0cb10acc717d4c1eaf3e4b87b83c643752a95b9d
Piyush Srivastava <piyush061996@gmail.com>
2018-06-27 04:31:17 +0530

    Added mongoDB and stored the urls with twitterId in database

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 
 var express = require("express");
 var app = express();
+var db = '';
+
+//initializing mongoDB
+const {MongoClient, ObjectID} = require('mongodb'); //creates variables MongoClient and ObjectID with values in mongodb object
+
+
+//connect to database
+MongoClient.connect('mongodb://localhost:27017',(err, client) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    db = client.db('OneDirectApp');
+});
+
+
+
 
 //getting the session
 var session = require('express-session');
@@ -31,29 +48,38 @@ var passport = require('passport')
   },
   function(token, tokenSecret, profile, cb) {
     if (profile) {
+      
         var screen_name = profile;
-        oauth.get( 'https://api.twitter.com/1.1/statuses/home_timeline.json?tweet_mode=extended&screen_name=' + screen_name + '&count=20'
+        oauth.get( 'https://api.twitter.com/1.1/statuses/home_timeline.json?tweet_mode=extended&screen_name=' + screen_name + '&count=5'
                   , token
                   , tokenSecret
                   , function (e, data, result){
                       if (e) console.error(e); 
-                      for(i=0;i<20;i++)
+                       for(i=0;i<5;i++)
                        {
-                           console.log((JSON.parse(data))[i].user['name']);
-                           console.log((JSON.parse(data))[i].full_text);
-                           if(((JSON.parse(data))[i].entities['urls']).length>0)
-                              console.log(((JSON.parse(data))[i].entities['urls'][0])['expanded_url']);
-                           console.log('-----------------------------------------');
+                        if(((JSON.parse(data))[i].entities['urls']).length>0){
+                           db.collection('Urls').insertOne({
+                             TwitterId: (JSON.parse(data))[i].user['screen_name'],
+                             TweetContentUrl: ((JSON.parse(data))[i].entities['urls'][0])['expanded_url']
+                           },(err, result) => {
+                            if(err){
+                                return  console.log('Unable to insert url', err);
+                            }
+                            console.log(JSON.stringify(result.ops,undefined, 2));
+                          });
                         }
-                    
+                      }
                   });
+                    
+                   }
     
-        }
+                  
         else {
             console.log("Authentication failed.")
         }
   }
 ));
+
 
 //to redirect to authentication page of twitter
 app.get('/auth/twitter',
